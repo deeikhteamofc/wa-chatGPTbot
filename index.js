ce1567deef007c20fb20c059cefbf452fe009bab
✅ A L P H A <94374721+I-am-ALPHA@users.noreply.github.com>
2023-03-09 16:58:50 +0530

    fixes

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const {
   proto,
   getContentType,
 } = require("@adiwajshing/baileys");
-const { default: aiIncConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
 const pino = require('pino')
 const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`);
 const pino = require("pino");
